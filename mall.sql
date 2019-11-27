529fe870751172d84f16b24d23e8502cb8047762
heyguojing <guo_ali@aliyun.com>
2019-11-27 17:50:27 +0800

    sql

---
+++ mall.sql
@@ -0,0 +1,40 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : php
+Source Server Version : 50721
+Source Host           : localhost:3306
+Source Database       : mall
+
+Target Server Type    : MYSQL
+Target Server Version : 50721
+File Encoding         : 65001
+
+Date: 2019-11-27 17:49:26
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for mall_admin
+-- ----------------------------
+DROP TABLE IF EXISTS `mall_admin`;
+CREATE TABLE `mall_admin` (
+  `admin_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '管理员id',
+  `username` varchar(60) NOT NULL DEFAULT '' COMMENT '用户名',
+  `password` varchar(32) NOT NULL DEFAULT '' COMMENT '密码',
+  `salt` varchar(6) DEFAULT '',
+  `status` tinyint(6) unsigned DEFAULT '0' COMMENT '管理员状态，0表示关闭，1表示开启',
+  `login_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `login_ip` varchar(40) NOT NULL DEFAULT '0.0.0.0' COMMENT '登陆ip',
+  `add_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `update_time` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`admin_id`),
+  UNIQUE KEY `用户名` (`username`),
+  KEY `用户状态` (`status`) COMMENT '用户状态\r\n'
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of mall_admin
+-- ----------------------------
+INSERT INTO `mall_admin` VALUES ('1', 'admin', '3d53b999d53504c6c3a4dec950d0deb3', 'cce536', '0', '1574846278', '127.0.0.1', '0', '0');
