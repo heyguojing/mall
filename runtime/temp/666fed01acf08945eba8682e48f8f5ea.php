92e8bde6becda1b89ef9c504b4a784243742e3c7
heyguojing <guo_ali@aliyun.com>
2019-12-11 09:57:21 +0800

    log

--- runtime/temp/666fed01acf08945eba8682e48f8f5ea.php
+++ runtime/temp/666fed01acf08945eba8682e48f8f5ea.php
@@ -1,7 +1,8 @@
-<?php /*a:1:{s:59:"C:\wamp\www\mall\application\admin\view\rbac\edit_node.html";i:1575466917;}*/ ?>
+<?php /*a:1:{s:59:"C:\wamp\www\mall\application\admin\view\rbac\edit_node.html";i:1575537685;}*/ ?>
 <!DOCTYPE html>
 <html lang="zh-CN">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -27,19 +28,19 @@
                     <a class="active" href="javascript:;">编辑<?php echo htmlentities($type); ?></a>
                 </li>
             </ul>
-       </div>
+        </div>
         <form action="" method="post" class="form form-horizontal">
             <div class="Operate_cont clearfix">
                 <label class="form-label"><?php echo htmlentities($type); ?>名称：</label>
                 <div class="formControls ">
-                    <input type="text" class="input-text" value="<?php echo htmlentities($editdata['name']); ?>" placeholder=""  name="name" datatype="*" nullmsg="<?php echo htmlentities($type); ?>名称不能为空！">
+                    <input type="text" class="input-text" value="<?php echo htmlentities($editdata['name']); ?>" placeholder="" name="name" datatype="*" nullmsg="<?php echo htmlentities($type); ?>名称不能为空！">
                     <span class="Validform_checktip"></span>
                 </div>
             </div>
             <div class="Operate_cont clearfix">
                 <label class="form-label"><?php echo htmlentities($type); ?>描述：</label>
                 <div class="formControls ">
-                    <input type="text" class="input-text" value="<?php echo htmlentities($editdata['title']); ?>" placeholder=""  name="title" datatype="*" nullmsg="<?php echo htmlentities($type); ?>描述不能为空！">
+                    <input type="text" class="input-text" value="<?php echo htmlentities($editdata['title']); ?>" placeholder="" name="title" datatype="*" nullmsg="<?php echo htmlentities($type); ?>描述不能为空！">
                     <span class="Validform_checktip"></span>
                 </div>
             </div>
@@ -61,10 +62,10 @@
                 <label class="form-label text-inline">状态：</label>
                 <div class="formControls form-inline">
                     <label for="">
-                        <input type="radio" name="status" value="1" <?php if($editdata['status'] == 1): ?> checked <?php endif; ?> >显示
+                        <input type="radio" name="status" value="1" <?php if($editdata['status'] == 1): ?> checked <?php endif; ?>>显示
                     </label>
                     <label for="">
-                        <input type="radio" name="status" value="0" <?php if($editdata['status'] == 0): ?> checked <?php endif; ?> >隐藏
+                        <input type="radio" name="status" value="0" <?php if($editdata['status'] == 0): ?> checked <?php endif; ?>>隐藏
                     </label>
                 </div>
             </div>
@@ -72,20 +73,20 @@
                 <label class="form-label text-inline">是否显示在菜单栏：</label>
                 <div class="formControls form-inline">
                     <label for="">
-                        <input type="radio" name="is_show" value="1" <?php if($editdata['is_show'] == 1): ?> checked <?php endif; ?> >显示
+                        <input type="radio" name="is_show" value="1" <?php if($editdata['is_show'] == 1): ?> checked <?php endif; ?>>显示
                     </label>
                     <label for="">
-                        <input type="radio" name="is_show" value="0" <?php if($editdata['is_show'] == 0): ?> checked <?php endif; ?> >隐藏
+                        <input type="radio" name="is_show" value="0" <?php if($editdata['is_show'] == 0): ?> checked <?php endif; ?>>隐藏
                     </label>
                 </div>
             </div>
             <div class="Operate_cont clearfix">
                 <label class="form-label">排序：</label>
                 <div class="formControls ">
-                    <input type="text" class="input-text" value="<?php echo htmlentities($editdata['sort']); ?>" placeholder=""  name="sort" datatype="*" nullmsg="排序名称不能为空！">
+                    <input type="text" class="input-text" value="<?php echo htmlentities($editdata['sort']); ?>" placeholder="" name="sort" datatype="*" nullmsg="排序名称不能为空！">
                     <span class="Validform_checktip"></span>
                 </div>
-            </div>            
+            </div>
             <div class="Operate_cont clearfix">
                 <input type="hidden" name="level" value="<?php echo htmlentities($level); ?>">
                 <input type="hidden" name="pid" value="<?php echo htmlentities($pid); ?>">
@@ -96,20 +97,21 @@
 
     <script type="text/javascript" src="/static/admin/Widget/Validform/5.3.2/Validform.min.js"></script>
     <script type="text/javascript">
-         $(function() {
+        $(function () {
             //表单验证
             $("form").Validform({
-                tiptype:function(msg,o,cssctl){
-                    if(!o.obj.is("form")){
+                tiptype: function (msg, o, cssctl) {
+                    if (!o.obj.is("form")) {
                         //默认表单
-                        var objtip=o.obj.parents(".formControls").find(".Validform_checktip");
-                        cssctl(objtip,o.type);
+                        var objtip = o.obj.parents(".formControls").find(".Validform_checktip");
+                        cssctl(objtip, o.type);
                         objtip.text(msg);
                     }
                 },
-                showAllError:true
+                showAllError: true
             })
         });
     </script>
 </body>
+
 </html>
